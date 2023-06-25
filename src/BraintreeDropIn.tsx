e569825dc4d1bc5b748414f0d0b38c0214335524
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-25 16:35:50 +0100

    .

--- src/BraintreeDropIn.tsx
+++ src/BraintreeDropIn.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import dropin from "braintree-web-drop-in";
+import * as Linking from 'expo-linking';
 import { Button } from "reactstrap";
 import './index.css';
 
@@ -7,12 +8,21 @@ const BraintreeDropIn = (props: any) => {
     const { onPaymentCompleted } = props;
     const braintreeInstance = useRef<dropin.Dropin | undefined>();
     const [authorization, setAuthorization] = useState('');
+    const [error, setError] = useState<string | null>(null);
 
     // Request client token from server when component mounts
     useEffect(() => {
         fetch('https://candii4-backend2-3f9abaacb350.herokuapp.com/client_token')
-            .then(res => res.text()) // Parse the response as text
-            .then(setAuthorization); // Update state with client token
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Failed to fetch client token");
+                }
+                return res.text();
+            }) // Parse the response as text
+            .then(setAuthorization) // Update state with client token
+            .catch(error => {
+                setError(error.message);
+            });
     }, []);
 
     useEffect(() => {
@@ -49,12 +59,26 @@ const BraintreeDropIn = (props: any) => {
         }
     };
 
+    const handleBackClick = () => {
+        // Redirect back to your app
+        Linking.openURL('your-app-scheme://ConfirmationPage');
+    };
+
     return (
         <div id="container">
-            <div id="braintree-drop-in-div" />
-            <Button className="braintreePayButton" color="primary" disabled={!braintreeInstance.current} onClick={handleClick}>
-                Pay
-            </Button>
+            {error ? (
+                <div>
+                    <p>An error occurred. Try again!</p>
+                    <Button onClick={handleBackClick}>Back to App</Button>
+                </div>
+            ) : (
+                <>
+                    <div id="braintree-drop-in-div" />
+                    <Button className="braintreePayButton" color="primary" disabled={!braintreeInstance.current} onClick={handleClick}>
+                        Pay
+                    </Button>
+                </>
+            )}
         </div>
     );
 };
