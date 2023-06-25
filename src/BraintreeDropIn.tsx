8f929c73447155f8df85dd9bcedaa8c6a027c5a6
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-25 13:47:32 +0100

    Update BraintreeDropIn.tsx

--- src/BraintreeDropIn.tsx
+++ src/BraintreeDropIn.tsx
@@ -1,31 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import dropin from "braintree-web-drop-in";
 import { Button } from "reactstrap";
 import './index.css';
 
 const BraintreeDropIn = (props: any) => {
-    const { show, onPaymentCompleted } = props;
-    const [braintreeInstance, setBraintreeInstance] = useState<dropin.Dropin | undefined>(undefined);
+    const { onPaymentCompleted } = props;
+    const braintreeInstance = useRef<dropin.Dropin | null>(null);
+    const [authorization, setAuthorization] = useState('');
+
+    // Request client token from server when component mounts
+    useEffect(() => {
+        fetch('https://candii4-backend2-3f9abaacb350.herokuapp.com/client_token')
+            .then(res => res.text()) // Parse the response as text
+            .then(setAuthorization); // Update state with client token
+    }, []);
 
     useEffect(() => {
-        if (show) {
+        if (authorization) {
             const initializeBraintree = () => {
                 dropin.create({
-                    authorization: "sandbox_s9gd7m2p_vp62s592633kc5p5",
+                    authorization,
                     container: '#braintree-drop-in-div',
                 }, (error, instance) => {
                     if (error) console.error(error);
-                    else setBraintreeInstance(instance);
+                    else braintreeInstance.current = instance;
                 });
             };
 
-            braintreeInstance ? braintreeInstance.teardown().then(initializeBraintree) : initializeBraintree();
+            if (braintreeInstance.current) {
+                braintreeInstance.current.teardown().then(initializeBraintree);
+            } else {
+                initializeBraintree();
+            }
         }
-    }, [show]);
+    }, [authorization]);
 
     const handleClick = () => {
-        if (braintreeInstance) {
-            braintreeInstance.requestPaymentMethod((error: any, payload: any) => {
+        if (braintreeInstance.current) {
+            braintreeInstance.current.requestPaymentMethod((error: any, payload: any) => {
                 if (error) console.error(error);
                 else {
                     const paymentMethodNonce = payload.nonce;
@@ -38,9 +50,9 @@ const BraintreeDropIn = (props: any) => {
     };
 
     return (
-        <div style={{ display: show ? "block" : "none" }}>
+        <div>
             <div id="braintree-drop-in-div" />
-            <Button className="braintreePayButton" color="primary" disabled={!braintreeInstance} onClick={handleClick}>
+            <Button className="braintreePayButton" color="primary" disabled={!braintreeInstance.current} onClick={handleClick}>
                 Pay
             </Button>
         </div>
